d0096ee750d1a587c1d958b99700fc2456cd0998
Yousaf112 <yyousafahmedd@gmail.com>
2022-12-01 23:51:18 +0500

    login and sign up done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Homepage />} />
+          </Route>
+          <Route path="/login">
+            <Route index element={<LoginPage />} />
+          </Route>
+          <Route path="/signup">
+            <Route index element={<SignupPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      gray_greenish: "#429b93",
+    },
     extend: {},
   },
   plugins: [],
