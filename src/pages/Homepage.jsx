e4aa14f7f91a445ed29efa8261dd8704a55716c1
Yousaf112 <yyousafahmedd@gmail.com>
2022-12-02 21:24:35 +0500

    Homepage update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <div>
-      <button href="/SignupPageRestaurants">SignUp As Restaurant</button>
-      <br />
-      <button href="/SignupPageNGO">SignUp As NGO</button>
+      <Link to="/SignupForRestaurants">
+        <button class="justify-center bg-gray_greenish hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Register your Restaurant
+        </button>
+      </Link>
+      <Link to="/SignupForNGO">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Register your NGO
+        </button>
+      </Link>
     </div>
   );
 };
